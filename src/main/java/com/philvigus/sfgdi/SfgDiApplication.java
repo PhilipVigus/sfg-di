d7f300153c27ab0e11ecf1be138a930219d6010a
PhilipVigus <philipvigus@yahoo.co.uk>
2021-12-02 07:57:24 +0000

    Add a simple controller
    This demonstrates a simple use of the Spring
    application context, which creates and manages the
    controller for you.

--- src/main/java/com/philvigus/sfgdi/SfgDiApplication.java
+++ src/main/java/com/philvigus/sfgdi/SfgDiApplication.java
@@ -1,13 +1,27 @@
 package com.philvigus.sfgdi;
 
+import com.philvigus.sfgdi.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SfgDiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SfgDiApplication.class, args);
+        ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
+
+        /**
+         * The spring framework creates an instance of the controller for you from
+         * inside the spring context.
+         *
+         * The framework is effectively managing the construction of the controller
+         */
+        MyController myController = (MyController) ctx.getBean("myController");
+
+        String greeting = myController.sayHello();
+
+        System.out.println(greeting);
     }
 
 }
