58c519a56e13090c75bfcf4faa318a52c36723b7
PhilipVigus <philipvigus@yahoo.co.uk>
2021-12-03 07:32:10 +0000

    Implement DI using properties

---
+++ src/main/java/com/philvigus/sfgdi/controllers/PropertyInjectedController.java
@@ -0,0 +1,12 @@
+package com.philvigus.sfgdi.controllers;
+
+import com.philvigus.sfgdi.services.GreetingService;
+
+public class PropertyInjectedController {
+    public GreetingService greetingService;
+
+    public String getGreeting() {
+        return greetingService.sayGreeting();
+
+    }
+}
