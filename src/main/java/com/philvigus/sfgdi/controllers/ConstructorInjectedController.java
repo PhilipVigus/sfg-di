daf09fdfac61714349cb60ed8e61dbbec76ae3bf
PhilipVigus <philipvigus@yahoo.co.uk>
2021-12-03 07:41:20 +0000

    Implement DI using constructor injection

---
+++ src/main/java/com/philvigus/sfgdi/controllers/ConstructorInjectedController.java
@@ -0,0 +1,15 @@
+package com.philvigus.sfgdi.controllers;
+
+import com.philvigus.sfgdi.services.GreetingService;
+
+public class ConstructorInjectedController {
+    private final GreetingService greetingService;
+
+    public ConstructorInjectedController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String getGreeting() {
+        return greetingService.sayGreeting();
+    }
+}
