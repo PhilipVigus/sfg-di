719f5a8151df41d1f2e9587b4fc3c68d43813d44
PhilipVigus <philipvigus@yahoo.co.uk>
2021-12-04 17:15:41 +0000

    Implement a demonstration of how to use a profile

--- src/main/java/com/philvigus/sfgdi/SfgDiApplication.java
+++ src/main/java/com/philvigus/sfgdi/SfgDiApplication.java
@@ -1,9 +1,6 @@
 package com.philvigus.sfgdi;
 
-import com.philvigus.sfgdi.controllers.ConstructorInjectedController;
-import com.philvigus.sfgdi.controllers.MyController;
-import com.philvigus.sfgdi.controllers.PropertyInjectedController;
-import com.philvigus.sfgdi.controllers.SetterInjectedController;
+import com.philvigus.sfgdi.controllers.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -19,18 +16,21 @@ public class SfgDiApplication {
          * inside the spring context.
          *
          * The framework is effectively managing the construction of the controller
-         */
-        System.out.println("---------------- Primary");
-        MyController myController = (MyController) ctx.getBean("myController");
-        System.out.println(myController.sayHello());
-
-        /**
-         * The following Spring-managed DI examples reply on the appropriate annotations
+         *
+         * Spring-managed DI examples rely on the appropriate annotations
          *      @Controller, @Autowired and @Service
          *
          *      Note that there is little functional difference between @Controller
          *      and @Service annotations - they're more to signal intent
          */
+
+        I18nController i18nController = (I18nController) ctx.getBean("i18nController");
+        System.out.println(i18nController.sayHello());
+
+        System.out.println("---------------- Primary");
+        MyController myController = (MyController) ctx.getBean("myController");
+        System.out.println(myController.sayHello());
+
         System.out.println("---------------- Property");
         PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
         System.out.println(propertyInjectedController.getGreeting());
