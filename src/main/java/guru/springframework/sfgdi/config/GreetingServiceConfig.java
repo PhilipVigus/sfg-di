0c761b996100ac5d13b019001b6cd529c68fe536
PhilipVigus <philipvigus@yahoo.co.uk>
2021-12-24 16:26:41 +0000

    Implement primary beans and properties with Java configuration

--- src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -1,10 +1,10 @@
 package guru.springframework.sfgdi.config;
 
-import guru.springframework.sfgdi.services.ConstructorGreetingService;
-import guru.springframework.sfgdi.services.PropertyInjectedGreetingService;
-import guru.springframework.sfgdi.services.SetterInjectedGreetingService;
+import guru.springframework.sfgdi.services.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class GreetingServiceConfig {
@@ -12,6 +12,25 @@ public class GreetingServiceConfig {
      * The method names you choose here are extremely important, as they
      * become the names of the beans in the Spring context
      */
+
+    @Profile("EN")
+    @Bean
+    I18nEnglishGreetingService i18nService() {
+        return new I18nEnglishGreetingService();
+    }
+
+    @Profile({"ES", "default"})
+    @Bean("i18nService")    // overrides the default bean name, which is usually the method name
+    I18NSpanishService i18NSpanishService() {
+        return new I18NSpanishService();
+    }
+
+    @Primary
+    @Bean
+    PrimaryGreetingService primaryGreetingService() {
+        return new PrimaryGreetingService();
+    }
+
     @Bean
     ConstructorGreetingService constructorGreetingService() {
         return new ConstructorGreetingService();
