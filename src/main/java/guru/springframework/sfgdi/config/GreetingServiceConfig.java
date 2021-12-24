17f4019311ce2c1206ad256eac79cab49b8f3ee9
PhilipVigus <philipvigus@yahoo.co.uk>
2021-12-24 16:39:19 +0000

    Demonstrate Java configured dependency injection

--- src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -1,5 +1,7 @@
 package guru.springframework.sfgdi.config;
 
+import guru.springframework.sfgdi.repositories.EnglishGreetingRepository;
+import guru.springframework.sfgdi.repositories.EnglishGreetingRepositoryImpl;
 import guru.springframework.sfgdi.services.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,10 +15,20 @@ public class GreetingServiceConfig {
      * become the names of the beans in the Spring context
      */
 
+    @Bean
+    EnglishGreetingRepository englishGreetingRepository() {
+        return new EnglishGreetingRepositoryImpl();
+    }
+
+    /**
+     * The important thing to note here is that we don't have to instantiate an instance of
+     * EnglishGreetingRepository ourselves. It is managed by Spring in the same way it would
+     * be if we were using annotations on the service classes themselves
+     */
     @Profile("EN")
     @Bean
-    I18nEnglishGreetingService i18nService() {
-        return new I18nEnglishGreetingService();
+    I18nEnglishGreetingService i18nService(EnglishGreetingRepository englishGreetingRepository) {
+        return new I18nEnglishGreetingService(englishGreetingRepository);
     }
 
     @Profile({"ES", "default"})
